3ff98f81478b9f79455557122b3220a839992981
khushal parmar <khushalparmar1208@gmail.com>
2025-01-17 08:36:28 +0530

    vite react folder understanding

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+    // can return only one element , so in jsx we use empty element
+    <>
+    <Chai/>
+    <h1>Chai aur react with vite | HC</h1>
+    <p>hi i am paragraph</p>
+    </>
+  )
+}
+
+export default App
